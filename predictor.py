31f057310b4b261e91cc51f451a29c934b30d612
vdircio <vdirci2@illinois.edu>
2024-02-04 16:55:05 -0600

    installed dependencies

--- predictor.py
+++ predictor.py
@@ -1,2 +1,11 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+# estimators - built-in machine learning algorithms and models
+# estimators can be fitted to some data using its fit method
+
+clf = RandomForestClassifier(random_state = 0)
+X = [[ 1,  2,  3],  # 2 samples, 3 features
+     [11, 12, 13]]
+y = [0, 1]  # classes of each sample
+clf.fit(X, y)
 
