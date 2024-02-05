560e0eb6dabe3bc3e2d737e5b4fbeec3565e03e6
vdircio <vdirci2@illinois.edu>
2024-02-04 20:57:15 -0600

    weird

--- predictor.py
+++ predictor.py
@@ -1,11 +1,21 @@
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from urllib.request import urlretrieve
 # estimators - built-in machine learning algorithms and models
 # estimators can be fitted to some data using its fit method
 
-clf = RandomForestClassifier(random_state = 0)
-X = [[ 1,  2,  3],  # 2 samples, 3 features
-     [11, 12, 13]]
-y = [0, 1]  # classes of each sample
-clf.fit(X, y)
+nba_url = 'https://www.kaggle.com/datasets/vivovinco/2023-2024-nba-player-stats?select=2023-2024+NBA+Player+Stats+-+Regular.csv'
+try:
+    df = pd.read_csv(nba_url)
+    print(df.head())
+except Exception as e:
+    print("Error:", e)
+
+
+# clf = RandomForestClassifier(random_state = 0)
+# X = [[ 1,  2,  3],  # 2 samples, 3 features
+#      [11, 12, 13]]
+# y = [0, 1]  # classes of each sample
+# clf.fit(X, y)
 
