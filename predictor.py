55ab0165b926cfdf014e736c9e33ec42e35d5fc2
vdircio <vdirci2@illinois.edu>
2024-02-04 21:48:38 -0600

    a start

--- predictor.py
+++ predictor.py
@@ -5,12 +5,14 @@ from urllib.request import urlretrieve
 # estimators - built-in machine learning algorithms and models
 # estimators can be fitted to some data using its fit method
 
-nba_url = 'https://www.kaggle.com/datasets/vivovinco/2023-2024-nba-player-stats?select=2023-2024+NBA+Player+Stats+-+Regular.csv'
-try:
-    df = pd.read_csv(nba_url)
-    print(df.head())
-except Exception as e:
-    print("Error:", e)
+pd.set_option('display.max_rows', 10)  # Maximum number of rows to display
+pd.set_option('display.max_columns', 10)  # Maximum number of columns to display
+pd.set_option('display.width', 100)  # Width of the display in characters
+
+
+csv_file_path = "2023-2024NBAPlayerStats.csv"
+df = pd.read_csv(csv_file_path, encoding='latin1')
+    
 
 
 # clf = RandomForestClassifier(random_state = 0)
